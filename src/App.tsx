94d68a5801b3badb87222ab252685c8f1b854da3
Almaz Balbaev <kujitengu@gmail.com>
2022-10-26 18:32:04 +0600

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Card from "./lib/Card";
+import CardView from "./CardView/CardView";
+import CardDeck from "./lib/CardDeck";
+import PokerHand from "./lib/PokerHand";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [cards, setCards] = useState<Card[]>([]);
+  const [combination, setCombination] = useState<string>("");
+
+  const startRound = () => {
+    const round = new CardDeck();
+    const outcome = round.getCards(5);
+    setCards(outcome);
+
+    const pokerHand = new PokerHand(outcome);
+    setCombination(pokerHand.getOutcome());
+  };
+
+  if (cards.length === 0) {
+    return (
+      <button onClick={startRound}>Play</button>
+    )
+  } else {
+    return (
+      <div className="App">
+        <button onClick={startRound}>Play</button>
+        <div className="playingCards faceImages">
+          <CardView rank={cards[0].rank} suit={cards[0].suit} />
+          <CardView rank={cards[1].rank} suit={cards[1].suit} />
+          <CardView rank={cards[2].rank} suit={cards[2].suit} />
+          <CardView rank={cards[3].rank} suit={cards[3].suit} />
+          <CardView rank={cards[4].rank} suit={cards[4].suit} />
+        </div>
+        <span>{combination}</span>
+      </div>
+    );
+  }
 }
 
 export default App;
